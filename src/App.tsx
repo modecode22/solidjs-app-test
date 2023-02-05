c468807e4ddc6d3cd4bac9619434097f48dd389c
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-02-05 15:44:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Component, createSignal } from 'solid-js';
+
+const App: Component = () => {
+  createSignal
+  return (
+    <p class="text-4xl text-green-700 text-center py-20">
+      redsfgesdrg
+    </p>
+  );
+};
+
+export default App;
